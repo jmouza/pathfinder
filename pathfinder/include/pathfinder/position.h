16a6049c44f43dd0182643e131f8368b0d6c9bee
jmouza <jawad.mouza@hotmail.com>
2025-06-01 12:31:59 +0200

    position ==

--- pathfinder/include/pathfinder/position.h
+++ pathfinder/include/pathfinder/position.h
@@ -1,10 +1,10 @@
+#pragma once
+
 struct Position 
 {
     unsigned int x;
     unsigned int y;
 
-    Position(unsigned int x, unsigned int y) {
-        this->x = x;
-        this->y = y;
-    }
+    Position(unsigned int x, unsigned int y): x(x), y(y) {}
+    bool operator== (const Position &other) const {return this->x == other.x && this->y == other.y;}
 };
