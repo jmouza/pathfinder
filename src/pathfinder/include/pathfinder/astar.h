f4fbf1006d543c0b84a5be4108500bd3d009f4cd
jmouza <jawad.mouza@hotmail.com>
2025-07-17 18:39:34 +0200

    cleaned astar

--- src/pathfinder/include/pathfinder/astar.h
+++ src/pathfinder/include/pathfinder/astar.h
@@ -9,6 +9,18 @@ public:
     const PathfinderResult Execute() override;
 
 private:
+    const double INFINITY = std::numeric_limits<double>::infinity();
+    SetOfNodes open_set;
+    std::unordered_map<Node, double, Node::HashFunction> distances_from_start_node; /* g(n): Distance from start node */
+    std::unordered_map<Node, double, Node::HashFunction> f_scores; /* distances_from_start_node[n] + distance_to_finish[n] */  
+    std::unordered_map<Node, Node, Node::HashFunction> came_from;
+    SetOfPositions explored_positions;
+
     int DistanceBetweenNodes(Node source, Node target) const;
+    void PopulateGAndFScores();
+    void PopulateExploredPositions();
+    Node NextNodeToExplore() const;
+    SetOfNodes NodesWithLowestFScores() const;
+    void HandleNeighbors(Node current_node);
     void ConstructPath();
 };
--- src/pathfinder/src/astar.cpp
+++ src/pathfinder/src/astar.cpp
@@ -11,98 +11,28 @@
 const PathfinderResult AStar::Execute() {
     if (!grid) throw std::runtime_error("Grid not set!");
 
-    const double INFINITY = std::numeric_limits<double>::infinity();
-
     Node start_node = grid->GetStartNode();
     Node finish_node = grid->GetFinishNode();
-    auto nodes = grid->GetNodes();
 
-    SetOfNodes open_set;
     open_set.insert(start_node);
 
-    std::unordered_map<Node, double, Node::HashFunction> g_scores; /* Distance to start node */
-    std::unordered_map<Node, int, Node::HashFunction> f_scores; /* g_scores[n] + distance_to_finish[n] */
-
-    for (auto node: nodes) {
-        g_scores.insert({node, INFINITY});
-        f_scores.insert({node, INFINITY});
-    }
-    g_scores.insert_or_assign(start_node, 0);
+    PopulateGAndFScores();
+    distances_from_start_node.insert_or_assign(start_node, 0);
     f_scores.insert_or_assign(start_node, DistanceBetweenNodes(start_node, finish_node));
 
-    std::unordered_map<Node, Node, Node::HashFunction> came_from;
-
-    SetOfPositions explored_positions;
-
     while (!open_set.empty()) {
-        /* Explored positions */
-        for (auto node: open_set) {
-            explored_positions.insert(node.GetPosition());
-        }
-        result.explored_steps.push_back(explored_positions);
+        PopulateExploredPositions();
 
-        /* Node in open_set with lowest fscore */
-        SetOfNodes nodes_with_lowest_fcost;
-        double min_distance = INFINITY;
-        for (auto node: open_set) {
-            if (f_scores.at(node) < min_distance) {
-                min_distance = f_scores.at(node);
-                nodes_with_lowest_fcost.clear();
-                nodes_with_lowest_fcost.insert(node);
-            } else if (f_scores.at(node) == min_distance) {
-                nodes_with_lowest_fcost.insert(node);
-            }
-        }
-        std::optional<Node> closest_node;
-        min_distance = INFINITY;
-        for (auto node: nodes_with_lowest_fcost) {
-            if (DistanceBetweenNodes(node, finish_node) < min_distance) {
-                min_distance = DistanceBetweenNodes(node, finish_node);
-                closest_node = node;
-            }
-        }
-        assert(closest_node);
+        Node current_node = NextNodeToExplore();
 
-        /* Check for finish node */
-        if (closest_node == finish_node) {
+        if (current_node == finish_node) {
             result.found_path = true;
-
-            /* construct path */
-            std::vector<Position> sequence;
-            Node node = grid->GetFinishNode();
-            Node came_from_node = came_from.at(node);
-
-            sequence.push_back(node.GetPosition());
-            while (came_from.count(came_from_node) != 0) {
-                sequence.push_back(came_from_node.GetPosition());
-
-                node = came_from_node;
-                came_from_node = came_from.at(node);
-            }
-            sequence.push_back(grid->GetStartNode().GetPosition());
-
-            std::reverse(sequence.begin(), sequence.end());
-
-            result.path = sequence;
-
+            ConstructPath();
             break;
         }
 
-        open_set.erase(*closest_node);
-
-        /* handle neighbors */
-        for (auto neighbor: grid->GetAdjacentNodes(*closest_node)) {
-            double tentative_gscore = g_scores.at(*closest_node) + DistanceBetweenNodes(*closest_node, neighbor);
-            if (tentative_gscore < g_scores.at(neighbor)) {
-                came_from.insert_or_assign(neighbor, *closest_node);
-                g_scores.insert_or_assign(neighbor, tentative_gscore);
-                f_scores.insert_or_assign(neighbor, tentative_gscore + DistanceBetweenNodes(neighbor, finish_node));
-
-                if (open_set.count(neighbor) == 0) {
-                    open_set.insert(neighbor);
-                }
-            }
-        }
+        open_set.erase(current_node);
+        HandleNeighbors(current_node);
     }
 
     return result;
@@ -118,6 +48,88 @@ int AStar::DistanceBetweenNodes(Node source, Node target) const {
     return dx + dy;
 }
 
+void AStar::PopulateGAndFScores() {
+    auto nodes = grid->GetNodes();
+
+    for (auto node: nodes) {
+        distances_from_start_node.insert({node, INFINITY});
+        f_scores.insert({node, INFINITY});
+    }
+}
+
+void AStar::PopulateExploredPositions() {
+    for (auto node: open_set) {
+        explored_positions.insert(node.GetPosition());
+    }
+    result.explored_steps.push_back(explored_positions);
+}
+
+SetOfNodes AStar::NodesWithLowestFScores() const {
+    SetOfNodes nodes_with_lowest_fcost;
+    double lowest_f_score = INFINITY;
+
+    for (auto node: open_set) {
+        if (f_scores.at(node) < lowest_f_score) {
+            lowest_f_score = f_scores.at(node);
+            nodes_with_lowest_fcost.clear();
+            nodes_with_lowest_fcost.insert(node);
+        } else if (f_scores.at(node) == lowest_f_score) {
+            nodes_with_lowest_fcost.insert(node);
+        }
+    }
+
+    return nodes_with_lowest_fcost;
+}
+
+Node AStar::NextNodeToExplore() const {
+    std::optional<Node> next_node;
+    Node finish_node = grid->GetFinishNode();
+    double lowest_h_score = INFINITY;
+
+    for (auto node: NodesWithLowestFScores()) {
+        double tentative_h_score = DistanceBetweenNodes(node, finish_node); 
+        if (tentative_h_score < lowest_h_score) {
+            lowest_h_score = tentative_h_score;
+            next_node = node;
+        }
+    }
+
+    return *next_node;
+}
+
+void AStar::HandleNeighbors(Node current_node) {
+    Node finish_node = grid->GetFinishNode();
+
+    for (auto neighbor: grid->GetAdjacentNodes(current_node)) {
+        double tentative_g_score = distances_from_start_node.at(current_node) + DistanceBetweenNodes(current_node, neighbor);
+        if (tentative_g_score < distances_from_start_node.at(neighbor)) {
+            came_from.insert_or_assign(neighbor, current_node);
+            
+            distances_from_start_node.insert_or_assign(neighbor, tentative_g_score);
+            f_scores.insert_or_assign(neighbor, tentative_g_score + DistanceBetweenNodes(neighbor, finish_node));
+
+            if (open_set.count(neighbor) == 0) {
+                open_set.insert(neighbor);
+            }
+        }
+    }
+}
+
 void AStar::ConstructPath() {
-    return;
+    std::vector<Position> sequence;
+    Node node = grid->GetFinishNode();
+    Node came_from_node = came_from.at(node);
+
+    sequence.push_back(node.GetPosition());
+    while (came_from.count(came_from_node) != 0) {
+        sequence.push_back(came_from_node.GetPosition());
+
+        node = came_from_node;
+        came_from_node = came_from.at(node);
+    }
+    sequence.push_back(grid->GetStartNode().GetPosition());
+
+    std::reverse(sequence.begin(), sequence.end());
+
+    result.path = sequence;
 }
